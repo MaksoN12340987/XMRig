70201875dc93b581364e4b1c8e2b5748a0916606
Maksim Andreev <andrianov_maksim@outlook.com>
2024-01-01 20:45:25 +0300

    New cotns, new repositories

---
+++ 5700g/xmrig-6.21.0_Maxeter/Maxeter.cmd
@@ -0,0 +1,4 @@
+:start
+xmrig.exe -a ghostrider --url stratum-eu.rplant.xyz:17028 --tls --user MGGb1E2U5uyWHqeuRuxGPZ2suaUtPPrquX.5700g --pass 
+goto start=15
+pause
---
+++ README.md
@@ -0,0 +1,4 @@
+# XMPig
+This repositorys for remember files miners
+
+https://github.com/MaksoN12340987/XMPig.git
