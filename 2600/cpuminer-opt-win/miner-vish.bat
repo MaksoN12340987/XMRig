59b47917b0fca30af1ab1ed8ca77de03e3defcca
Maksim Andreev <andrianov_maksim@outlook.com>
2024-01-15 21:39:20 +0300

    2600 vish vitch 5 treads

--- 2600/cpuminer-opt-win/miner-vish.bat
+++ 2600/cpuminer-opt-win/miner-vish.bat
@@ -1,4 +1,4 @@
 rem
 :start
-"%~dp0"cpuminer-sse2amd.exe -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17079 -u uoWkHtKCWzZsxrAx2tb7c5Bf4rfSMP8LWo.2600x -p start=6
+"%~dp0"cpuminer-sse2amd.exe -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17079 -u uoWkHtKCWzZsxrAx2tb7c5Bf4rfSMP8LWo.2600x -p start=5
 goto start
