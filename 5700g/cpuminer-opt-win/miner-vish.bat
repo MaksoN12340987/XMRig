5bcd1392bfa348f06e3c01289016a3c49f37d226
Maksim Andreev <andrianov_maksim@outlook.com>
2024-01-15 14:12:57 +0300

    5700п vish vitch 7 treads

--- 5700g/cpuminer-opt-win/miner-vish.bat
+++ 5700g/cpuminer-opt-win/miner-vish.bat
@@ -1,4 +1,4 @@
 rem
 :start
-"%~dp0"cpuminer-sse2amd.exe -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17079 -u uoWkHtKCWzZsxrAx2tb7c5Bf4rfSMP8LWo.5700g -p start=8
+"%~dp0"cpuminer-sse2amd.exe -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17079 -u uoWkHtKCWzZsxrAx2tb7c5Bf4rfSMP8LWo.5700g -p start=7
 goto start
