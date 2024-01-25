5238d4d2907a660a65b19af05b8ed26a72f5346b
Maksim Andreev <andrianov_maksim@otlook.com>
2024-01-26 02:29:19 +0300

    update miner files for linux

--- 2600/Linux/cpuminer-opt-linux/miner-vish.sh
+++ 2600/Linux/cpuminer-opt-linux/miner-vish.sh
@@ -7,6 +7,6 @@
 #
 FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while [ 1 ]; do
-"$FOLDER"/cpuminer-sse2amd -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17079 -u uoWkHtKCWzZsxrAx2tb7c5Bf4rfSMP8LWo.5700g -p start=8
+"$FOLDER"/cpuminer-sse2amd -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17079 -u uoWkHtKCWzZsxrAx2tb7c5Bf4rfSMP8LWo.2600x -p start=8
 sleep 5
 done
