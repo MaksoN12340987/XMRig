e52a95ebd2245030414a50901feff899ed7cb78f
Maksim Andreev <andrianov_maksim@outlook.com>
2024-01-30 23:03:08 +0300

    smartiecoin is compleated run

--- 2600/cpuminer-opt-win/smartiecoin.bat
+++ 2600/cpuminer-opt-win/smartiecoin.bat
@@ -1,4 +1,4 @@
 rem
 :start
-"%~dp0"cpuminer-sse2.exe -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17111 -u aKxpsHptCst6R5uyRwUZr2YXaNp1XH3sRp.2600x
+"%~dp0"cpuminer-sse2amd.exe -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17111 -u aKxpsHptCst6R5uyRwUZr2YXaNp1XH3sRp.2600x
 goto start
