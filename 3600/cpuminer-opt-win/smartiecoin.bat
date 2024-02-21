ecbfa9b4db68bad6ba702e1a315d6e19811bdee0
Maksim Andreev <andrianov_maksim@outlook.com>
2024-02-21 18:05:20 +0300

    3600 )))

--- 3600/cpuminer-opt-win/smartiecoin.bat
+++ 3600/cpuminer-opt-win/smartiecoin.bat
@@ -1,4 +1,4 @@
 rem
 :start
-"%~dp0"cpuminer-sse2amd.exe -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17111 -u aLixvdFRYHzrGZJKAc1xdH4V3PqKaawRAA.2600x
+"%~dp0"cpuminer-sse2amd.exe -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17111 -u aLixvdFRYHzrGZJKAc1xdH4V3PqKaawRAA.3600
 goto start
