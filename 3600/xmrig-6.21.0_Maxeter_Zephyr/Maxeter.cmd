936d05e18e7852582c5f38096f73657ed3ca09ce
Maksim Andreev <andrianov_maksim@outlook.com>
2024-02-21 17:49:19 +0300

    3600 )))

---
+++ 3600/xmrig-6.21.0_Maxeter_Zephyr/Maxeter.cmd
@@ -0,0 +1,4 @@
+:start
+xmrig.exe -a ghostrider --url stratum-eu.rplant.xyz:17028 --tls --user MGGb1E2U5uyWHqeuRuxGPZ2suaUtPPrquX.2600x --pass 
+goto start=12
+pause
--- vidioCards/lolminer-windows-1.83/readme.txt
+++
@@ -1,3 +0,0 @@
-Please replace YOUR_POOL_ID inside mine_gram.bat file.
-
-Пожалуйста замените YOUR_POOL_ID в файле mine_gram.bat
