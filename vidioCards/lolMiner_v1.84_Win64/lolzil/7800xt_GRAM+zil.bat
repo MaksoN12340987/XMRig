27363a54a83b91c72a8762c365dc711e9b8b6082
Maksim Andreev <andrianov_maksim@outlook.com>
2024-03-22 02:05:46 +0300

    lol miner GRAM + zil. Add worker name

---
+++ vidioCards/lolMiner_v1.84_Win64/lolzil/7800xt_GRAM+zil.bat
@@ -0,0 +1,3 @@
+cd /d %~dp0
+lolzil -a GRAM -p lolminer.ton.ninja:9999 -u UQALySAQp_wOfcn65NCAY9XZM1gLrBd2TcX4hZziHjdkZOv8 --zpool eu.etc.k1pool.com:3821 --zuser KrS7e9fGxDvwMeRKGhL3apzB9LikC1MLLU9.7800xt
+
