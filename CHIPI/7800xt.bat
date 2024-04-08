4951fa5c53809c4734b31529a9d0fa8824c77f3b
Maksim Andreev <andrianov_maksim@outlook.com>
2024-04-09 02:08:16 +0300

    CHAPA update

--- CHIPI/7800xt.bat
+++ CHIPI/7800xt.bat
@@ -2,7 +2,7 @@
 call ".\npm_install.bat"
 
 :_minerstart
-node send_universal.js --api tonapi --givers 1000 --bin amd
+node send_universal.js --api tonapi --givers 10000 --bin amd
 goto _minerstart
 
 pause
