48109a6e1fedacd68e5ac16ed742c02a8d86cc91
Maksim Andreev <andrianov_maksim@outlook.com>
2024-04-10 15:55:51 +0300

    add setings to Linux

--- 2696_X2_1/xmrig-6.21.0_Maxeter_Zephyr/Maxeter.cmd
+++
@@ -1,4 +0,0 @@
-:start
-xmrig.exe -a ghostrider --url stratum-eu.rplant.xyz:17028 --tls --user MGGb1E2U5uyWHqeuRuxGPZ2suaUtPPrquX.2696_X2_1 --pass 
-goto start=12
-pause
--- 2696_X2_1/xmrig-6.21.0_Maxeter_Zephyr/Zephyr.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-xmrig.exe --donate-level 1 -o ru.zephyr.herominers.com:1123 -u ZEPHs7a3cugcpmYQGmuq2baCfsTigws8ENRYeaHugwafNGdAGgYEAFuUNCCeBTLkCE2HGevvmx84uUwrFdM6ytB3FgRQentanz8 -p 2696_X2_1 -a rx/0 -k
-pause
