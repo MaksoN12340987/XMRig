5b0ed9d24f8bc3af67fcfaa5c37ea812dfa70565
Maxim Andreev <andrianov_maksim@outlook.com>
2024-04-22 17:42:46 +0300

    upgrade lolminer to version 1.88 + hyperpool setings

--- vidioCards/lolMiner/lolzil/6800_GRAM+zil.bat
+++ vidioCards/lolMiner/lolzil/6800_GRAM+zil.bat
@@ -1,3 +1,2 @@
 cd /d %~dp0
-lolzil -a GRAM -p lolminer.ton.ninja:9999 -u UQALySAQp_wOfcn65NCAY9XZM1gLrBd2TcX4hZziHjdkZOv8 --zpool eu.etc.k1pool.com:3821 --zuser KrS7e9fGxDvwMeRKGhL3apzB9LikC1MLLU9.6800
-
+lolzil -a GRAM -p lolminer.hyperpool.pro:8040 --ton-mode 6 -u eaf89db7108470dc3f6b23eac007561a4e5cf994979b5638b19edf0932b54548df98ffa69e9f --zpool eu.etc.k1pool.com:3821 --zuser KrS7e9fGxDvwMeRKGhL3apzB9LikC1MLLU9.6800
