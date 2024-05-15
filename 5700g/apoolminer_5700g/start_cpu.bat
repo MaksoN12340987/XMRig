9273c724f1592d2b2ba4e261b1a4c3f489919218
Maksim Andreev <andrianov_maksim@outlook.com>
2024-05-16 01:43:43 +0300

    update qubic apool

--- 5700g/apoolminer_5700g/start_cpu.bat
+++ 5700g/apoolminer_5700g/start_cpu.bat
@@ -1,2 +1,2 @@
-apoolminer.exe --account CP_32hb86nvxc --gpu-off --pool qubic2.hk.apool.io:3334
+apoolminer.exe --account CP_uv87383wcb --gpu-off --pool qubic1.hk.apool.io:3334 --worker 5700g -t 16
 pause
---
+++ Qubic/apoolminer_win_v1.4.0/start_all.bat
@@ -0,0 +1,2 @@
+apoolminer.exe --account CP_uv87383wcb --pool qubic2.hk.apool.io:3334 --worker3700x
+pause
