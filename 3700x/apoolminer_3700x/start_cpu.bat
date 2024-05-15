9d168ee7ee032cc28059af983813fdf2377c2967
Maksim Andreev <andrianov_maksim@outlook.com>
2024-05-16 01:23:12 +0300

    update qubic apool

---
+++ 3700x/apoolminer_3700x/start_cpu.bat
@@ -0,0 +1,2 @@
+apoolminer.exe --account CP_uv87383wcb --gpu-off --pool qubic2.hk.apool.io:3334 --worker 3700x
+pause
---
+++ 3700x/apoolminer_3700x/start_gpu.bat
@@ -0,0 +1,2 @@
+apoolminer.exe --account CP_32hb86nvxc --cpu-off --pool qubic2.hk.apool.io:3334
+pause
